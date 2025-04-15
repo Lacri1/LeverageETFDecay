168d7cb07ce5ec065762b612e9a0c06b39c16937
Lacri1 <shinju1102@gmail.com>
2025-04-15 10:44:36 +0900

    Add mobile support

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",  // React 및 TypeScript 파일을 포함
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '640px',   // 작은 화면 (모바일)
+        'md': '768px',   // 중간 크기 (태블릿)
+        'lg': '1024px',  // 큰 화면 (데스크탑)
+        'xl': '1280px',  // 더 큰 화면
+        '2xl': '1536px', // 매우 큰 화면
+      },
+    },
   },
   plugins: [],
 }
